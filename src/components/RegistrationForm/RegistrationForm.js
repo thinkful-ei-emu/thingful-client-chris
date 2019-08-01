cddffb3388e81beb0495b2b50506d73149a05e54
Chris Martin <chris.bizunit34@gmail.com>
2019-08-01 18:26:07 +0000

    added registration functionality

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import AuthApiService from '../../services/auth-api-service'
 import { Button, Input, Required } from '../Utils/Utils'
 
 export default class RegistrationForm extends Component {
@@ -12,15 +13,24 @@ export default class RegistrationForm extends Component {
     ev.preventDefault()
     const { full_name, nick_name, user_name, password } = ev.target
 
-    console.log('registration form submitted')
-    console.log({ full_name, nick_name, user_name, password })
-
+    this.setState({ error: null })
+    AuthApiService. postUser({
+      user_name: user_name.value,
+      password: password.value,
+      full_name: full_name.value,
+      nickname: nick_name.value
+    })
+    .then(user => {
     full_name.value = ''
     nick_name.value = ''
     user_name.value = ''
     password.value = ''
     this.props.onRegistrationSuccess()
-  }
+  })
+  .catch(res => {
+    this.setState({ error: res.error })
+  });
+}
 
   render() {
     const { error } = this.state
